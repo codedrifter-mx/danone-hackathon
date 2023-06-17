9241a26918bbac06d23e610735fae37d2f126409
alfredo-flores <alfredo.f.g.156@gmail.com>
2023-06-17 11:24:04 -0600

    MLP sklearn 0.52

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import json
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -58,38 +56,14 @@ def extract_features(data):
     return features, labels
 
 
-def train_model(features, labels, model_name):
+def train_model(features, labels):
     vectorizer = DictVectorizer()
     X = vectorizer.fit_transform(features)
     y = labels
 
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    if model_name == "RandomForestClassifier":
-        model = RandomForestClassifier()
-        param_grid = {
-            'n_estimators': [100, 200, 300],
-            'max_depth': [None, 5, 10],
-            'min_samples_split': [2, 5, 10],
-            'min_samples_leaf': [1, 2, 4],
-            'max_features': ['sqrt', 'log2']
-        }
-        search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='f1_macro', cv=5)
-    elif model_name == "MLPClassifier":
-        model = MLPClassifier(max_iter=500)
-        param_grid = {
-            'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
-            'activation': ['relu', 'tanh'],
-            'alpha': [0.0001, 0.001, 0.01]
-        }
-        search = RandomizedSearchCV(estimator=model, param_distributions=param_grid, scoring='f1_macro', cv=5, n_iter=10)
-    else:
-        raise ValueError("Invalid model name")
-
-    search.fit(X_train, y_train)
-    best_params = search.best_params_
-    best_score = search.best_score_
-    model.set_params(**best_params)
+    model = MLPClassifier(max_iter=500)  # Increase max_iter value to 500
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_val)
@@ -102,40 +76,26 @@ def train_model(features, labels, model_name):
 train_data = preprocess_data('train_products.json')
 train_features, train_labels = extract_features(train_data)
 
-# Train the models
-models = {
-    "RandomForestClassifier": {},
-    "GradientBoostingClassifier": {},
-    "MLPClassifier": {}
-}
-
-for model_name in models:
-    model, vectorizer, f1_macro = train_model(train_features, train_labels, model_name)
-    models[model_name]["model"] = model
-    models[model_name]["vectorizer"] = vectorizer
-    models[model_name]["f1_macro"] = f1_macro
+# Train the MLPClassifier model
+model, vectorizer, f1_macro = train_model(train_features, train_labels)
 
 # Preprocess the test data
 test_data = preprocess_data('test_products.json')
 test_features, _ = extract_features(test_data)
 
-# Generate predictions for each model
+# Generate predictions using the MLPClassifier model
 predictions_dict = {}
 
-for model_name, model_info in models.items():
-    model = model_info["model"]
-    vectorizer = model_info["vectorizer"]
+X_test = vectorizer.transform(test_features)
+predictions = model.predict(X_test)
 
-    X_test = vectorizer.transform(test_features)
-    predictions = model.predict(X_test)
+# Create a dictionary in the desired format
+model_predictions = {"target": {}}
+for i, prediction in enumerate(predictions):
+    model_predictions["target"][str(i)] = int(prediction)
 
-    # Create a dictionary in the desired format
-    model_predictions = {"target": {}}
-    for i, prediction in enumerate(predictions):
-        model_predictions["target"][str(i)] = int(prediction)
+predictions_dict["MLPClassifier"] = model_predictions
 
-    predictions_dict[model_name] = model_predictions
-
-    # Save predictions to a file
-    with open(f'model_{model_name}_predictions.json', 'w') as file:
-        json.dump(model_predictions, file)
+# Save predictions to a file
+with open('predictions.json', 'w') as file:
+    json.dump(model_predictions, file)
